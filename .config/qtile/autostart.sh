b7942bb6da2b099bde49beab50c35d25f74c821f
WillChamness <wzchamness@gmail.com>
2023-06-09 01:22:58 -0400

    Revert 555bd6a

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# run compositor in background
 picom -b
-# prevent PC from sleeping (sleeps when running emulators)
 xset s off
 xset -dpms
 xset s noblank
-# monitor setup
-xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 --output DisplayPort-0 --mode 1920x1080 --rate 60 --right-of HDMI-A-0
-# not sure what this is but flatpaks run a lot faster when this is ran
-dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
+xrandr --output HDMI-A-0 --mode 1920x1080 --output DisplayPort-0 --mode 1920x1080 --right-of HDMI-A-0
