c9a29fa52a5de466418382c229f8066362d27cbf
WillChamness <wzchamness@gmail.com>
2023-07-10 23:01:46 -0400

    update autostart.sh
    add lines to allow flatpak applications to load faster

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,3 +4,9 @@ xset s off
 xset -dpms
 xset s noblank
 xrandr --output HDMI-A-0 --mode 1920x1080 --output DisplayPort-0 --mode 1920x1080 --right-of HDMI-A-0
+# does something with xdg-desktop-portal to allow flatpaks to initially load faster
+# dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
+systemctl --user import-environment DISPLAY XAUTHORITY
+if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+    dbus-update-activation-environment DISPLAY XAUTHORITY
+fi
