1cd1d8335a56e9666da0273980abc2d628d2d451
WillChamness <wzchamness@gmail.com>
2023-07-29 16:53:16 -0400

    add more qtile keys
    add keys to activate rofi scripts and key for print screen

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 Aldo Cortesi 
+# Copyright (c) 2010 Aldo Cortesi
 # Copyright (c) 2010, 2014 dequi
 # Copyright (c) 2012 Randall Ma
 # Copyright (c) 2012-2014 Tycho Andersen
@@ -37,13 +37,15 @@ import mycolors
 
 
 # Predefined variables
-mod = "mod4" # super key
+mod = "mod4"  # super key
 terminal = "alacritty"
 default_browser = "flatpak run io.gitlab.librewolf-community"
 default_media_player = "flatpak run org.videolan.VLC"
 colors = mycolors.init_colors()
-dmenu = "rofi -show run" # dmenu replacement
+dmenu = "rofi -show run"  # dmenu replacement
 calculator = "rofi -show calc -modi calc -no-show-match -no-sort"
+shutdown_menu = f"rofi -show power-menu -modi power-menu:{os.path.expanduser('~/.local/bin/rofi-power-menu')}"
+wifi_menu = os.path.expanduser("~/.local/bin/rofi-wifi-menu")  # custom shell script
 office_suite = "flatpak run org.libreoffice.LibreOffice"
 file_manager = "thunar"
 
@@ -56,17 +58,26 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    #Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -87,49 +98,80 @@ keys = [
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
     # My custom keys
+    # Interacting with screen
     Key([mod], "comma", lazy.prev_screen(), desc="Move to the previous monitor"),
     Key([mod], "period", lazy.next_screen(), desc="Move focus to next monitor"),
-    Key([mod, "shift"], "s", lazy.spawn("scrot -s /home/will/Pictures/'Arch-%m-%d-%Y-%s-screenshot.jpg'"), desc="take screenshot"),
     Key([mod, "shift"], "m", lazy.window.toggle_minimize(), desc="Toggle minimize"),
+    Key([mod, "shift"], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+    # Take screenshot
+    Key(
+        [mod, "shift"],
+        "s",
+        lazy.spawn(
+            f"scrot -s {os.path.expanduser('~/Pictures/')}/Arch-%m-%d-%Y-%s-screenshot.jpg"
+        ),
+        desc="take screenshot",
+    ),
+    Key(
+        ["shift"],
+        "Print",
+        lazy.spawn(
+            f"scrot {os.path.expanduser('~/Pictures/')}/Arch-%m-%d-%Y-%s-screenshot.jpg"
+        ),
+        desc="print screen",
+    ),
+    # Various apps
     Key([mod], "b", lazy.spawn(default_browser), desc="Librewolf"),
     Key([mod, "shift"], "b", lazy.spawn("flatpak run com.brave.Browser"), desc="Brave"),
     Key([mod], "m", lazy.spawn(default_media_player), desc="Media player"),
-    Key([mod], "space", lazy.spawn(dmenu), desc="Run dmenu or replacement"),
-    Key([mod], "c", lazy.spawn(calculator), desc="Run rofi calculator"),
     Key([mod], "o", lazy.spawn(office_suite), desc="Run LibreOffice suite"),
     Key([mod], "f", lazy.spawn(file_manager), desc="Run thunar file manager"),
-    Key([mod, "shift"], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+    # Rofi
+    Key([mod], "space", lazy.spawn(dmenu), desc="Run dmenu or replacement"),
+    Key([mod], "c", lazy.spawn(calculator), desc="Run rofi calculator"),
+    Key([mod], "w", lazy.spawn(wifi_menu), desc="Connect to WiFi"),
+    Key(
+        [mod, "control", "shift"],
+        "s",
+        lazy.spawn(shutdown_menu),
+        desc="Shutdown the system",
+    ),
 ]
 
 group_names = "CTRL,WEB1,WEB2,DOCS,FILE,DEV,GAME,PROD,ETC".split(",")
 groups = [Group(name) for name in group_names]
 for i in range(len(groups)):
-    keys.extend([
-        Key(
-            [mod],
-            str(i+1),
-            lazy.group[groups[i].name].toscreen(),
-            desc="Switch to group {}".format(groups[i].name)
-        ),
-        Key(
+    keys.extend(
+        [
+            Key(
+                [mod],
+                str(i + 1),
+                lazy.group[groups[i].name].toscreen(),
+                desc="Switch to group {}".format(groups[i].name),
+            ),
+            Key(
                 [mod, "shift"],
-                str(i+1),
+                str(i + 1),
                 lazy.window.togroup(groups[i].name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(groups[i].name)
-        ),
-        Key(
-            [mod, "control"],
-            str(i+1),
-            lazy.window.togroup(groups[i].name, switch_group=False),
-            desc="Move the focused window to group {} (but don't switch to it)".format(groups[i].name)
-        )
-    ])
+                desc="Switch to & move focused window to group {}".format(
+                    groups[i].name
+                ),
+            ),
+            Key(
+                [mod, "control"],
+                str(i + 1),
+                lazy.window.togroup(groups[i].name, switch_group=False),
+                desc="Move the focused window to group {} (but don't switch to it)".format(
+                    groups[i].name
+                ),
+            ),
+        ]
+    )
 
 layouts = [
-    #layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    #layout.Max(),
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    # layout.Max(),
     layout.MonadTall(border_width=2, border_focus=colors["purple"], margin=6)
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -145,10 +187,7 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="Ubuntu Bold",
-    fontsize=12,
-    padding=5,
-    background=colors["bg"]
+    font="Ubuntu Bold", fontsize=12, padding=5, background=colors["bg"]
 )
 
 extension_defaults = widget_defaults.copy()
@@ -163,9 +202,9 @@ screens = [
                 widget.GroupBox(
                     highlight_method="block",
                     rounded=False,
-                    active=colors["active-groups"], # window open in group ==> active
+                    active=colors["active-groups"],  # window open in group ==> active
                     this_current_screen_border=colors["purple"],
-                    background=colors["bg"]
+                    background=colors["bg"],
                 ),
                 widget.Prompt(),
                 widget.WindowName(foreground=colors["purple"]),
@@ -180,14 +219,16 @@ screens = [
                 widget.Sep(foreground=colors["white"]),
                 widget.CPU(foreground=colors["yellow"]),
                 widget.ThermalSensor(
-                    tag_sensor="Tctl", 
-                    threshold=90, 
+                    tag_sensor="Tctl",
+                    threshold=90,
                     format="{temp:.0f}{unit}",
-                    foreground=colors["yellow"]
+                    foreground=colors["yellow"],
                 ),
                 widget.Sep(foreground=colors["white"]),
                 widget.TextBox("Memory", foreground=colors["light-green"], padding=2),
-                widget.Memory(foreground=colors["light-green"], measure_mem="G", padding=2),
+                widget.Memory(
+                    foreground=colors["light-green"], measure_mem="G", padding=2
+                ),
                 widget.Chord(
                     chords_colors={
                         "launch": ("#ff0000", "#ffffff"),
@@ -199,14 +240,15 @@ screens = [
                 widget.Sep(foreground=colors["white"]),
                 widget.Wlan(foreground=colors["blue"], padding=10),
                 widget.Sep(foreground=colors["white"]),
-                widget.Clock(format="%a %m-%d-%Y | %I:%M %p", foreground=colors["light-blue"]),
+                widget.Clock(
+                    format="%a %m-%d-%Y | %I:%M %p", foreground=colors["light-blue"]
+                ),
             ],
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
-
     Screen(
         wallpaper="~/.config/qtile/wallpapers/person-looking-over-city-1920x1080.jpg",
         wallpaper_mode="stretch",
@@ -215,9 +257,9 @@ screens = [
                 widget.GroupBox(
                     highlight_method="block",
                     rounded=False,
-                    active=colors["active-groups"], # window open in group ==> active
+                    active=colors["active-groups"],  # window open in group ==> active
                     this_current_screen_border=colors["purple"],
-                    background=colors["bg"]
+                    background=colors["bg"],
                 ),
                 widget.Prompt(),
                 widget.WindowName(foreground=colors["purple"]),
@@ -231,14 +273,16 @@ screens = [
                 widget.Sep(foreground=colors["white"]),
                 widget.CPU(foreground=colors["yellow"]),
                 widget.ThermalSensor(
-                    tag_sensor="Tctl", 
-                    threshold=90, 
+                    tag_sensor="Tctl",
+                    threshold=90,
                     format="{temp:.0f}{unit}",
-                    foreground=colors["yellow"]
+                    foreground=colors["yellow"],
                 ),
                 widget.Sep(foreground=colors["white"]),
                 widget.TextBox("Memory", foreground=colors["light-green"], padding=2),
-                widget.Memory(foreground=colors["light-green"], measure_mem="G", padding=2),
+                widget.Memory(
+                    foreground=colors["light-green"], measure_mem="G", padding=2
+                ),
                 widget.Chord(
                     chords_colors={
                         "launch": ("#ff0000", "#ffffff"),
@@ -250,7 +294,9 @@ screens = [
                 widget.Sep(foreground=colors["white"]),
                 widget.Wlan(foreground=colors["blue"], padding=10),
                 widget.Sep(foreground=colors["white"]),
-                widget.Clock(format="%a %m-%d-%Y | %I:%M %p", foreground=colors["light-blue"]),
+                widget.Clock(
+                    format="%a %m-%d-%Y | %I:%M %p", foreground=colors["light-blue"]
+                ),
             ],
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
@@ -259,17 +305,25 @@ screens = [
     ),
 ]
 
+
 @hook.subscribe.startup_once
 def autostart():
-        home = os.path.expanduser('~/.config/qtile/autostart.sh')
-        subprocess.Popen([home])
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
+    subprocess.Popen([home])
 
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
     Click([mod], "Button1", lazy.window.toggle_floating()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
