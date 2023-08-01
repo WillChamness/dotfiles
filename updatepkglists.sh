1d3e845d1b9c24e41c7f3d38ff6b7ef6b0ff7791
WillChamness <wzchamness@gmail.com>
2023-08-01 19:40:02 -0400

    change pacman package list command
    prevent only explicitly installed programs from being listed

--- updatepkglists.sh
+++ updatepkglists.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # list installed packages from arch repos and AUR
-pacman -Qqen > pkglist.txt
+pacman -Qqn > pkglist.txt
 comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > optdeplist.txt
-pacman -Qqem > foreignpkglist.txt
+pacman -Qqm > foreignpkglist.txt
 
 # list installed flatpaks
 flatpak remotes --show-details | awk '{print $1,$3}' > flatpak_remotes.txt
