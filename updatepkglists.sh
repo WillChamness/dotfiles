2acdf56406f4bd253d3976dc24cf783220351f36
WillChamness <wzchamness@gmail.com>
2023-08-05 14:24:28 -0400

    update package lists
    add python packages installed through pipx

--- updatepkglists.sh
+++ updatepkglists.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # list installed packages from arch repos and AUR
-pacman -Qqn > pkglist.txt
+pacman -Qqen > pkglist.txt
 comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > optdeplist.txt
-pacman -Qqm > foreignpkglist.txt
+pacman -Qqem > foreignpkglist.txt
 
 # list installed flatpaks
 flatpak remotes --show-details | awk '{print $1,$3}' > flatpak_remotes.txt
 flatpak list --app --columns=application --columns=origin > flatpaks.txt | awk '{print $1,$2}' > flatpaks.txt
 
+# list installed packages with pip
+pipx list --short | awk '{print $1}' > python-pkglist.txt
