7c6fd6e76eba844639c8bc896736ad94a132d458
WillChamness <wzchamness@gmail.com>
2024-03-28 01:25:55 -0400

    update autostart script
    change xrandr monitor configuration and add comments

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# run compositor in background
 picom -b
+# prevent PC from sleeping (sleeps when running emulators)
 xset s off
 xset -dpms
 xset s noblank
-xrandr --output HDMI-A-0 --mode 1920x1080 --output DisplayPort-0 --mode 1920x1080 --right-of HDMI-A-0
-# does something with xdg-desktop-portal to allow flatpaks to initially load faster
+# monitor setup
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 --primary --output DisplayPort-0 --mode 1920x1080 --rate 60 --right-of HDMI-A-0
+# not sure what this is but flatpaks run a lot faster when this is ran
 # dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
 systemctl --user import-environment DISPLAY XAUTHORITY
 if command -v dbus-update-activation-environment >/dev/null 2>&1; then
